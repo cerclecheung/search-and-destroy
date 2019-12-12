8cb1e9b98d1d159aa6b9e93960cf299299f3ab22
Yuanhao Zhang <cercle@bu.edu>
2019-12-12 09:37:26 -0500

    all done

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
 
 // Complete this algo
 const minJumps = arr => {
-
+  let jumpCount = 1;
+  const alteredArr = arr.map((ele, idx) => ele + idx);
+  console.log("altered: ", alteredArr);
+  let currentIdx = 0;
+  while (currentIdx < arr.length - 1) {
+    let minIdx = currentIdx + 1;
+    let maxIdx = alteredArr[currentIdx];
+    if (maxIdx >= arr.length - 1) {
+      return jumpCount;
+    } else {
+      const subArr = alteredArr.slice(minIdx, maxIdx + 1);
+      let max = Math.max(...subArr);
+      currentIdx = subArr.lastIndexOf(max) + minIdx;
+      jumpCount++;
+      console.log("subArr: ", subArr, "max: ", max, "currentIdx: ", currentIdx);
+    }
+  }
+  // let idxOfMax = arr.lastIndexOf(maxValueOfArr);
 };
 
-module.exports = minJumps
+module.exports = minJumps;
